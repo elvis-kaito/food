a7a6f996a0602822e2db20ee101a4bd65c7cdf08
Elvis Kaito <namtran4194@gmail.com>
2020-07-11 18:50:51 +0700

    add search bar

---
+++ src/components/SearchBar.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { Text, View, StyleSheet } from "react-native";
+import { Feather } from "@expo/vector-icons";
+import { TextInput } from "react-native-gesture-handler";
+
+const SearchBar = ({ term, onTermChange }) => {
+  return (
+    <View style={styles.backgroundStyle}>
+      <Feather name="search" style={styles.iconStyle} />
+      <TextInput
+        style={styles.inputStyle}
+        placeholder="Search"
+        autoCapitalize="none"
+        autoCorrect={false}
+        value={term}
+        onChangeText={(newTerm) => onTermChange(newTerm)}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  backgroundStyle: {
+    backgroundColor: "#F0EEEE",
+    height: 50,
+    borderRadius: 5,
+    marginHorizontal: 15,
+    marginTop: 10,
+    flexDirection: "row",
+  },
+  inputStyle: {
+    fontSize: 18,
+    flex: 1,
+  },
+  iconStyle: {
+    fontSize: 35,
+    alignSelf: "center",
+    marginHorizontal: 15,
+  },
+});
+
+export default SearchBar;
